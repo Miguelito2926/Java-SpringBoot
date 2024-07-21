b43d9a28b6f6475496230484227f7715d30f3522
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-07-21 12:09:39 -0300

    implements delete using method map in classe Optional

--- src/main/java/com/ednaldo/ecommerce/api/controller/ClienteController.java
+++ src/main/java/com/ednaldo/ecommerce/api/controller/ClienteController.java
@@ -4,9 +4,11 @@ import com.ednaldo.ecommerce.domain.entity.Cliente;
 import com.ednaldo.ecommerce.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +40,27 @@ public class ClienteController {
     public ResponseEntity<Cliente> insert(@RequestBody Cliente cliente) {
         Cliente clienteSalvo = clienteRepository.save(cliente);
         return ResponseEntity.ok(clienteSalvo);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Optional<Cliente> clienteesixste = clienteRepository.findById(id);
+        if (clienteesixste.isPresent()) {
+            clienteRepository.delete(clienteesixste.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Object> clienteUpdate(@RequestBody Cliente cliente, @PathVariable Long id) {
+        return clienteRepository
+                .findById(id)
+                .map(clienteExistente -> {
+                    cliente.setId(clienteExistente.getId());
+                    clienteRepository.save(cliente);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }

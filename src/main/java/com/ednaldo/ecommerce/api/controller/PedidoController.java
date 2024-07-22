f549279df402b30bb652492bf64971f59e8d8dca
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-07-21 21:23:27 -0300

    implements service insertPedidos

--- src/main/java/com/ednaldo/ecommerce/api/controller/PedidoController.java
+++ src/main/java/com/ednaldo/ecommerce/api/controller/PedidoController.java
@@ -1,4 +1,27 @@
 package com.ednaldo.ecommerce.api.controller;
 
+import com.ednaldo.ecommerce.domain.dto.PedidoDTO;
+import com.ednaldo.ecommerce.domain.entity.Pedido;
+import com.ednaldo.ecommerce.service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping(value = "api/v1/pedidos")
 public class PedidoController {
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public Long insertPedido(@RequestBody PedidoDTO pedidoDTO) {
+        Pedido pedido = pedidoService.insert(pedidoDTO);
+        return pedido.getId();
+    }
 }
--- src/main/java/com/ednaldo/ecommerce/service/PedidoService.java
+++ src/main/java/com/ednaldo/ecommerce/service/PedidoService.java
@@ -1,12 +1,70 @@
 package com.ednaldo.ecommerce.service;
 
+import com.ednaldo.ecommerce.domain.dto.ItemPedidoDTO;
+import com.ednaldo.ecommerce.domain.dto.PedidoDTO;
+import com.ednaldo.ecommerce.domain.entity.Cliente;
+import com.ednaldo.ecommerce.domain.entity.ItemPedido;
+import com.ednaldo.ecommerce.domain.entity.Pedido;
+import com.ednaldo.ecommerce.domain.entity.Produto;
+import com.ednaldo.ecommerce.domain.repository.ClienteRepository;
+import com.ednaldo.ecommerce.domain.repository.ItemPedidoRepository;
 import com.ednaldo.ecommerce.domain.repository.PedidoRepository;
+import com.ednaldo.ecommerce.domain.repository.ProdutoRepository;
+import com.ednaldo.ecommerce.exception.RegraNegocioException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PedidoService {
 
     @Autowired
     private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
+
+    @Transactional
+    public Pedido insert(PedidoDTO pedidoDTO) {
+        Long idCliente = pedidoDTO.getClienteId();
+        Cliente cliente = clienteRepository.findById(idCliente)
+                .orElseThrow(() -> new RegraNegocioException("Código do cliente inválido."));
+
+        Pedido pedido = new Pedido();
+        pedido.setTotal(pedidoDTO.getTotal());
+        pedido.setDataPedido(LocalDate.now());
+        pedido.setCliente(cliente);
+
+        List<ItemPedido> itemPedido = converterItems(pedido, pedidoDTO.getItems());
+        pedidoRepository.save(pedido);
+        itemPedidoRepository.saveAll(itemPedido);
+        pedido.setItemPedidos(new HashSet<>(itemPedido));
+        return pedido;
+    }
+
+    public List<ItemPedido> converterItems(Pedido pedido, List<ItemPedidoDTO> items) {
+        if (items.isEmpty()) {
+            throw new RegraNegocioException("Não é possivel realizar um pedido sem  items.");
+        }
+        return items.stream().map(dtoItem -> {
+            Long idProduto = dtoItem.getProdutoId();
+            Produto produto = produtoRepository.findById(idProduto).orElseThrow(() -> new RegraNegocioException("Código do produto inválido: " + idProduto));
+            ItemPedido itemPedido = new ItemPedido();
+            itemPedido.setQuantidade(dtoItem.getQuantidade());
+            itemPedido.setPedido(pedido);
+            itemPedido.setProduto(produto);
+            return itemPedido;
+        }).collect(Collectors.toList());
+    }
 }

540fd6eccd2fc43aadfb2df57639de247693dae1
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-07-23 20:33:24 -0300

    feat: create query methods for repository requests

--- src/main/java/com/ednaldo/ecommerce/domain/repository/PedidoRepository.java
+++ src/main/java/com/ednaldo/ecommerce/domain/repository/PedidoRepository.java
@@ -3,10 +3,16 @@ package com.ednaldo.ecommerce.domain.repository;
 import com.ednaldo.ecommerce.domain.entity.Cliente;
 import com.ednaldo.ecommerce.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
     Set<Pedido> findByCliente(Cliente cliente);
+
+    @Query("select p from Pedido p left join fetch p.itens where p.id = :id")
+    Optional<Pedido> findByIdFetchItem(@Param("id") Long id);
 }
